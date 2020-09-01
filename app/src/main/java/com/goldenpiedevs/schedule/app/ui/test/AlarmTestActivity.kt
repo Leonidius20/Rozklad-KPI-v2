558629a960a8202e576f0f3b1d3c81ea1c06c537
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-09-01 20:40:17 +0300

    added scheduling testing

--- app/src/main/java/com/goldenpiedevs/schedule/app/ui/test/AlarmTestActivity.kt
+++ app/src/main/java/com/goldenpiedevs/schedule/app/ui/test/AlarmTestActivity.kt
@@ -4,11 +4,18 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import com.evernote.android.job.JobManager
+import com.evernote.android.job.JobRequest
+import com.evernote.android.job.util.support.PersistableBundleCompat
 import com.goldenpiedevs.schedule.app.R
 import com.goldenpiedevs.schedule.app.core.utils.work.ShowAlarmWork
 import com.goldenpiedevs.schedule.app.ui.alarm.AlarmActivity
+import kotlinx.android.synthetic.main.alarm_test_activity_layout.*
+import java.util.concurrent.TimeUnit
 
 class AlarmTestActivity : Activity() {
+
+    private var jobId = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,31 @@ class AlarmTestActivity : Activity() {
         startActivity(Intent(this, AlarmActivity::class.java).apply {
             putExtra(ShowAlarmWork.LESSON_NUMBER, 1)
         })
+    }
+
+    fun schedule(view: View) {
+        val TAG = "ShowAlarmWork"
+        val DAY_UUID = "day_uuid"
+        val LESSON_NUMBER = "lesson_number"
+
+        val dataBuilder = PersistableBundleCompat().apply {
+            putString(DAY_UUID, "fff")
+            putInt(LESSON_NUMBER, 1)
+        }
+
+        jobId = JobRequest.Builder(TAG)
+                .setExtras(dataBuilder)
+                .setPeriodic(
+                        TimeUnit.MINUTES.toMillis(periodTextView.text.toString().toLong()),
+                        TimeUnit.MINUTES.toMillis(flexMSTextView.text.toString().toLong()))
+                // .setPersisted(true)
+                .build()
+                .schedule()
+
+    }
+
+    fun cancel(view: View) {
+        JobManager.instance().cancel(jobId)
     }
 
 
